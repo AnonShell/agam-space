4edde4f7aee3a03d2624af23cbb6fa1cbf1c58d3
Ramesh Lingappan <ramesh@rameshl.com>
2026-01-03 21:34:43 +0100

    chore(web): removes unused methods

--- apps/web/src/store/trusted-devices.store.ts
+++ apps/web/src/store/trusted-devices.store.ts
@@ -12,27 +12,13 @@ interface TrustedDevicesStore {
   devices: TrustedDevice[];
   fetchDevices: () => Promise<void>;
   setDevices: (devices: TrustedDevice[]) => void;
-  registerDevice: (params: {
-    userId: string;
-    deviceName: string;
-    cmk: Uint8Array;
-  }) => Promise<void>;
-  removeDevice: (deviceId: string) => Promise<void>;
 }
 
-export const useTrustedDevicesStore = create<TrustedDevicesStore>()((set, get) => ({
+export const useTrustedDevicesStore = create<TrustedDevicesStore>()(set => ({
   devices: [],
   fetchDevices: async () => {
     const devices = await TrustedDevicesService.fetchDevices();
     set({ devices });
   },
   setDevices: devices => set({ devices }),
-  registerDevice: async ({ userId, deviceName, cmk }) => {
-    await TrustedDevicesService.registerDevice({ userId, deviceName, cmk });
-    await get().fetchDevices();
-  },
-  removeDevice: async deviceId => {
-    await TrustedDevicesService.removeDevice(deviceId);
-    await get().fetchDevices();
-  },
 }));
